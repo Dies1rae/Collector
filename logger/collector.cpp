496bdf83209e5f4b9ccdf38d96bdb2d803961da7
Николай Полтавский <n.poltavskiy@5-tv.ru>
2020-08-18 18:50:03 +0300

    start addding programm from winreg

--- logger/collector.cpp
+++ logger/collector.cpp
@@ -48,6 +48,7 @@ void collector::run() {
 		this->main_log_container_->add_log_string(ptr);
 	}
 	this->main_log_container_->add_log_string(get_pc_network_soft_info());
+	get_installed_software();
 
 
 	if (this->key_) {
@@ -242,7 +243,49 @@ std::string collector::get_pc_network_soft_info() {
 
 std::vector<std::string> collector::get_installed_software() {
 	std::vector<std::string> res;
-	
+	HKEY hUninstKey = NULL;
+	HKEY hAppKey = NULL;
+	WCHAR sAppKeyName[1024];
+	WCHAR sSubKey[1024];
+	WCHAR sDisplayName[1024];
+	//WCHAR* sRoot = L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall";
+	long lResult = ERROR_SUCCESS;
+	DWORD dwType = KEY_ALL_ACCESS;
+	DWORD dwBufferSize = 0;
+
+	//Open the "Uninstall" key.
+	if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\", 0, KEY_READ, &hUninstKey) != ERROR_SUCCESS){
+		std::cerr << "POSHEL NAHUY" << std::endl;
+		system("PAUSE");
+	}
+
+	for (DWORD dwIndex = 0; lResult == ERROR_SUCCESS; dwIndex++){
+		//Enumerate all sub keys...
+		dwBufferSize = sizeof(sAppKeyName);
+		if ((lResult = RegEnumKeyEx(hUninstKey, dwIndex, sAppKeyName,&dwBufferSize, NULL, NULL, NULL, NULL)) == ERROR_SUCCESS){
+			//Open the sub key.
+			wsprintf(sSubKey, L"%s\\%s", L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\", sAppKeyName);
+			if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, sSubKey, 0, KEY_READ, &hAppKey) != ERROR_SUCCESS){
+				RegCloseKey(hAppKey);
+				RegCloseKey(hUninstKey);
+				std::cerr << "POSHEL NAHUY" << std::endl;
+				system("PAUSE");
+			}
+
+			//Get the display name value from the application's sub key.
+			dwBufferSize = sizeof(sDisplayName);
+			if (RegQueryValueEx(hAppKey, L"DisplayName", NULL,&dwType, (unsigned char*)sDisplayName, &dwBufferSize) == ERROR_SUCCESS){
+				wprintf(L"%s\n", sDisplayName);
+			}
+			else {
+				//Display name value doe not exist, this application was probably uninstalled.
+			}
+
+			RegCloseKey(hAppKey);
+		}
+	}
+	RegCloseKey(hUninstKey);
+	return res;
 }
 
 void collector::collect_log_file(){
